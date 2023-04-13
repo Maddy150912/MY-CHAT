360b5a3fb95499074bd14c6d5be43628746fd445
Mandar Yangal <84185600+Maddy150912@users.noreply.github.com>
2023-04-13 13:15:35 +0530

    updated

--- backend_server/server.js
+++ backend_server/server.js
@@ -50,7 +50,7 @@ const server = app.listen(PORT, console.log(`server running ${PORT}`));
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000","https://mychat-x0qt.onrender.com"]
   },
 });
 
